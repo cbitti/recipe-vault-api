e85d319341760c4f9dbc532fffbf373e2a1514e9
Christopher Bitti <christopherbitti9@gmail.com>
2025-12-10 21:27:02 -0500

    feat: implement oauth2 login and jwt token generation

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timedelta, timezone
+from typing import Any
+import jwt
 from pwdlib import PasswordHash
+from app.core.config import settings
 
 password_hash = PasswordHash.recommended()
 
@@ -9,3 +13,23 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def get_password_hash(password: str) -> str:
     return password_hash.hash(password)
+
+
+def create_access_token(
+    subject: str | Any, expires_delta: timedelta | None = None
+) -> str:
+    """
+    Generates a JSON Web Token (JWT) with an expiration time.
+    """
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+
+    to_encode = {"exp": expire, "sub": str(subject)}
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
